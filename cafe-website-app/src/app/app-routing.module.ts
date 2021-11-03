86891e2385e6afa07ef74840ebc8a3ecb71e3fd0
Aanchi-glitch2744 <aanchalkumari2744@gmail.com>
2021-11-03 21:49:05 +0530

    Added components and static header and footer

--- cafe-website-app/src/app/app-routing.module.ts
+++ cafe-website-app/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './MyComponents/home-page/home-page.component';
+import { AboutPageComponent } from './MyComponents/about-page/about-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomePageComponent},  //Do not add slash for path in Angular
+  {path: 'about', component: AboutPageComponent}  //Display router about endpoint. Where? => Router outlet.
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- cafe-website-app/src/app/app.module.ts
+++ cafe-website-app/src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './MyComponents/home-page/home-page.component';
+import { AboutPageComponent } from './MyComponents/about-page/about-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    AboutPageComponent
   ],
   imports: [
     BrowserModule,
