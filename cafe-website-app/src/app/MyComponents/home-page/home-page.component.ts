40211591c886094d6d3e8f4b470c2c3c39d7aa77
Aanchi-glitch2744 <aanchalkumari2744@gmail.com>
2021-11-05 10:34:18 +0530

    Added structure, Lorem Ipsum dummy text and img

--- cafe-website-app/src/app/MyComponents/home-page/home-page.component.ts
+++ cafe-website-app/src/app/MyComponents/home-page/home-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-home-page',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
-  constructor() { }
+  constructor() { 
+    
+  }
 
   ngOnInit(): void {
   }
+
+  Testing() {
+    var name = $("txtName").val();
+    alert(name);
+  }
+
 
 }
